a94db04b4ec60d2eb729668a76432c69a2e848e4
Michael Kopp <kopp.michael@yahoo.de>
2020-12-11 22:19:30 +0100

    add day11, part 2

--- src/day11.ts
+++ src/day11.ts
@@ -19,25 +19,85 @@ class Floor implements Place
     kind: PlaceKind = PlaceKind.Floor;
 };
 
+// Strategy pattern for problem description
+interface ProblemDescription {
+    minRelevantSeatsOccupiedToClearSeat: number;
+    findRelevantSeatsForSeatAt(pos: {row: number, column: number}, room: Room): Array<Seat>;
+}
+
+export class Part1 implements ProblemDescription {
+    minRelevantSeatsOccupiedToClearSeat: number = 4;
+    findRelevantSeatsForSeatAt(pos: {row: number, column: number}, room: Room): Array<Seat>
+    {
+        let relevant_others: Array<Seat> = [];
+        for (let deltas of [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]) {
+            const neighbour_row = pos.row + deltas[0];
+            const neighbour_col = pos.column + deltas[1];
+            if (room.is_in_room(neighbour_row, neighbour_col)) {
+                let neighbour_place = room.place_at(neighbour_row, neighbour_col);
+                if (neighbour_place.kind == PlaceKind.Seat) {
+                    relevant_others.push(neighbour_place as Seat);
+                }
+            }
+
+        }
+        return relevant_others;
+    }
+}
+
+export class Part2 implements ProblemDescription {
+    minRelevantSeatsOccupiedToClearSeat: number = 5;
+    findRelevantSeatsForSeatAt(pos: {row: number, column: number}, room: Room): Array<Seat>
+    {
+        let relevant_others: Array<Seat> = [];
+        for (let direction of [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]) {
+            for (let n: number = 1; n < Math.max(room.columns, room.rows); ++n) {
+                const row = pos.row + n * direction[0];
+                const col = pos.column + n * direction[1];
+                const end_of_room_reached = ! room.is_in_room(row, col);
+                if (end_of_room_reached) {
+                    break;
+                }
+                else {
+                    let place = room.place_at(row, col);
+                    if (place.kind == PlaceKind.Floor) {
+                        continue; // continue in this direction (look over floor)
+                    }
+                    else if (place.kind == PlaceKind.Seat) {
+                        relevant_others.push(place as Seat);
+                        break; // done in this direction
+                    }
+                    else {
+                        throw new Error(`Unknown kind of place at ${row}, ${col}`);
+                    }
+
+                }
+
+            }
+        }
+        return relevant_others;
+    }
+}
+
 export class Seat implements Place
 {
     kind: PlaceKind = PlaceKind.Seat;
 
-    neighbours: Array<Seat> = [];
+    relevant_others: Array<Seat> = [];
     is_currently_occupied: boolean = false;
     is_occupied_after_update: boolean | null = null;
-    row: number = -1; // DEBUGGING
-    column: number = -1 // DEBUGGING
+    row: number = -1; // used for debugging only
+    column: number = -1 // used for debugging only
 
-    constructor() {
+    constructor(public problemDescription: ProblemDescription) {
     }
 
     compute_next_occupation(): void
     {
-        const number_of_occupied_neighbours = this.neighbours.reduce(
+        const number_of_occupied_neighbours = this.relevant_others.reduce(
             (num_occ: number, seat: Seat) => num_occ += seat.is_currently_occupied ? 1 : 0,
             0);
-        if (this.is_currently_occupied && (number_of_occupied_neighbours >= 4)) {
+        if (this.is_currently_occupied && (number_of_occupied_neighbours >= this.problemDescription.minRelevantSeatsOccupiedToClearSeat)) {
             this.is_occupied_after_update = false;
         }
         else if (! this.is_currently_occupied && (number_of_occupied_neighbours == 0)) {
@@ -71,16 +131,19 @@ export class Room
     places: Array<Place> = [];
     rows: number;
     columns: number;
+    problemDescription: ProblemDescription;
 
-    constructor(layout: Array<string>)
+    constructor(layout: Array<string>, problemDescription: ProblemDescription = new Part1())
     {
         this.columns = layout[0].length;
         this.rows = layout.length;
 
+        this.problemDescription = problemDescription;
+
         for (const row of layout) {
             for (const place_character of row) {
                 if (place_character === "L") {
-                    let seat = new Seat();
+                    let seat = new Seat(this.problemDescription);
                     this.seats.push(seat);
                     this.places.push(seat);
                 }
@@ -101,17 +164,7 @@ export class Room
                     let seat = place as Seat;
                     seat.row = row_index;
                     seat.column = col_index;
-                    for (let deltas of [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]) {
-                        const neighbour_row = row_index + deltas[0];
-                        const neighbour_col = col_index + deltas[1];
-                        if (this.is_in_room(neighbour_row, neighbour_col)) {
-                            let neighbour_place = this.place_at(neighbour_row, neighbour_col);
-                            if (neighbour_place.kind == PlaceKind.Seat) {
-                                seat.neighbours.push(neighbour_place as Seat);
-                            }
-                        }
-
-                    }
+                    seat.relevant_others = problemDescription.findRelevantSeatsForSeatAt({row: row_index, column: col_index}, this);
                 }
             }
         }
@@ -194,7 +247,7 @@ function update_until_no_futher_changes(room: Room): void
     var changed = true;
     while (changed) {
         changed = room.update();
-        console.log(room.number_of_occupied_seats());
+        // console.log(room.number_of_occupied_seats());
     }
 }
 
@@ -203,7 +256,14 @@ function update_until_no_futher_changes(room: Room): void
 if (require.main === module) {
     let input = read_file_of_strings("input/day11");
 
-    let room = new Room(input);
-    update_until_no_futher_changes(room);
-    console.log("Problem 1: ", room.number_of_occupied_seats());
+    {
+        let room = new Room(input);
+        update_until_no_futher_changes(room);
+        console.log("Problem 1: ", room.number_of_occupied_seats());
+    }
+    {
+        let room = new Room(input, new Part2());
+        update_until_no_futher_changes(room);
+        console.log("Problem 2: ", room.number_of_occupied_seats());
+    }
 }
--- tests/day11.test.ts
+++ tests/day11.test.ts
@@ -1,4 +1,5 @@
 import {
+    Part2,
     Room,
     Seat
 } from "../src/day11";
@@ -19,69 +20,68 @@ const input = [
     "L.LLLLL.LL",
 ];
 
-const input_with_1_iteration = [
-    "#.##.##.##",
-    "#######.##",
-    "#.#.#..#..",
-    "####.##.##",
-    "#.##.##.##",
-    "#.#####.##",
-    "..#.#.....",
-    "##########",
-    "#.######.#",
-    "#.#####.##",
-];
-
-const input_with_2_iterations = [
-    "#.LL.L#.##",
-    "#LLLLLL.L#",
-    "L.L.L..L..",
-    "#LLL.LL.L#",
-    "#.LL.LL.LL",
-    "#.LLLL#.##",
-    "..L.L.....",
-    "#LLLLLLLL#",
-    "#.LLLLLL.L",
-    "#.#LLLL.##",
-]
-
-const input_with_3_iterations = [
-    "#.##.L#.##",
-    "#L###LL.L#",
-    "L.#.#..#..",
-    "#L##.##.L#",
-    "#.##.LL.LL",
-    "#.###L#.##",
-    "..#.#.....",
-    "#L######L#",
-    "#.LL###L.L",
-    "#.#L###.##",
-]
-
-const input_with_4_iterations = [
-    "#.#L.L#.##",
-    "#LLL#LL.L#",
-    "L.L.L..#..",
-    "#LLL.##.L#",
-    "#.LL.LL.LL",
-    "#.LL#L#.##",
-    "..L.L.....",
-    "#L#LLLL#L#",
-    "#.LLLLLL.L",
-    "#.#L#L#.##",
-]
-
-const input_with_5_iterations = [
-    "#.#L.L#.##",
-    "#LLL#LL.L#",
-    "L.#.L..#..",
-    "#L##.##.L#",
-    "#.#L.LL.LL",
-    "#.#L#L#.##",
-    "..L.L.....",
-    "#L#L##L#L#",
-    "#.LLLLLL.L",
-    "#.#L#L#.##",
+const input_with_part1_update = [
+    input,
+    [
+        "#.##.##.##",
+        "#######.##",
+        "#.#.#..#..",
+        "####.##.##",
+        "#.##.##.##",
+        "#.#####.##",
+        "..#.#.....",
+        "##########",
+        "#.######.#",
+        "#.#####.##",
+    ],
+    [
+        "#.LL.L#.##",
+        "#LLLLLL.L#",
+        "L.L.L..L..",
+        "#LLL.LL.L#",
+        "#.LL.LL.LL",
+        "#.LLLL#.##",
+        "..L.L.....",
+        "#LLLLLLLL#",
+        "#.LLLLLL.L",
+        "#.#LLLL.##",
+    ],
+    [
+        "#.##.L#.##",
+        "#L###LL.L#",
+        "L.#.#..#..",
+        "#L##.##.L#",
+        "#.##.LL.LL",
+        "#.###L#.##",
+        "..#.#.....",
+        "#L######L#",
+        "#.LL###L.L",
+        "#.#L###.##",
+    ],
+    [
+        "#.#L.L#.##",
+        "#LLL#LL.L#",
+        "L.L.L..#..",
+        "#LLL.##.L#",
+        "#.LL.LL.LL",
+        "#.LL#L#.##",
+        "..L.L.....",
+        "#L#LLLL#L#",
+        "#.LLLLLL.L",
+        "#.#L#L#.##",
+    ],
+    [
+        "#.#L.L#.##",
+        "#LLL#LL.L#",
+        "L.#.L..#..",
+        "#L##.##.L#",
+        "#.#L.LL.LL",
+        "#.#L#L#.##",
+        "..L.L.....",
+        "#L#L##L#L#",
+        "#.LLLLLL.L",
+        "#.#L#L#.##",
+    ]
 ]
 
 test("Read and dump input", () => {
@@ -95,13 +95,13 @@ function test_correct_neighbours(room: Room, place_index: { row: number, col: nu
         indices => room.place_at(indices.row, indices.col) as Seat
     )
     let place = room.place_at(place_index.row, place_index.col) as Seat
-    expect(isPermutationOf(place.neighbours, neighbours)).toBe(true);
+    expect(isPermutationOf(place.relevant_others, neighbours)).toBe(true);
 }
 
-test("Correct neighbours", () => {
+test("Part 1: Correct neighbours", () => {
     let room = new Room(input);
     expect(isPermutationOf(
-        (room.place_at(0, 0) as Seat).neighbours,
+        (room.place_at(0, 0) as Seat).relevant_others,
         [room.place_at(1, 0) as Seat, room.place_at(1, 1) as Seat]
     )).toBe(true);
 
@@ -113,15 +113,122 @@ test("update", () => {
     let room = new Room(input);
     expect(room.dump()).toEqual(input);
     expect(room.update()).toBe(true);
-    expect(room.dump()).toEqual(input_with_1_iteration);
+    expect(room.dump()).toEqual(input_with_part1_update[1]);
     expect(room.update()).toBe(true);
-    expect(room.dump()).toEqual(input_with_2_iterations);
+    expect(room.dump()).toEqual(input_with_part1_update[2]);
     expect(room.update()).toBe(true);
-    expect(room.dump()).toEqual(input_with_3_iterations);
+    expect(room.dump()).toEqual(input_with_part1_update[3]);
     expect(room.update()).toBe(true);
-    expect(room.dump()).toEqual(input_with_4_iterations);
+    expect(room.dump()).toEqual(input_with_part1_update[4]);
     expect(room.update()).toBe(true);
-    expect(room.dump()).toEqual(input_with_5_iterations);
+    expect(room.dump()).toEqual(input_with_part1_update[5]);
     expect(room.update()).toBe(false);
-    expect(room.dump()).toEqual(input_with_5_iterations); // no change!
+    expect(room.dump()).toEqual(input_with_part1_update[5]); // no change!
+});
+
+
+// Part 2
+
+const input_with_part2_update =
+    [
+        input,
+        [
+            "#.##.##.##",
+            "#######.##",
+            "#.#.#..#..",
+            "####.##.##",
+            "#.##.##.##",
+            "#.#####.##",
+            "..#.#.....",
+            "##########",
+            "#.######.#",
+            "#.#####.##",
+        ], [
+            "#.LL.LL.L#",
+            "#LLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLL#",
+            "#.LLLLLL.L",
+            "#.LLLLL.L#",
+        ], [
+            "#.L#.##.L#",
+            "#L#####.LL",
+            "L.#.#..#..",
+            "##L#.##.##",
+            "#.##.#L.##",
+            "#.#####.#L",
+            "..#.#.....",
+            "LLL####LL#",
+            "#.L#####.L",
+            "#.L####.L#",
+        ], [
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##LL.LL.L#",
+            "L.LL.LL.L#",
+            "#.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#",
+        ], [
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##L#.#L.L#",
+            "L.L#.#L.L#",
+            "#.L####.LL",
+            "..#.#.....",
+            "LLL###LLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#",
+        ], [
+            "#.L#.L#.L#",
+            "#LLLLLL.LL",
+            "L.L.L..#..",
+            "##L#.#L.L#",
+            "L.L#.LL.L#",
+            "#.LLLL#.LL",
+            "..#.L.....",
+            "LLL###LLL#",
+            "#.LLLLL#.L",
+            "#.L#LL#.L#",
+        ]
+    ];
+
+
+test("Part 2: Correct neighbours", () => {
+    let room = new Room(input, new Part2());
+    test_correct_neighbours(room, {row: 0, col: 0}, [{row: 1, col: 0}, {row: 1, col: 1}, {row: 0, col: 2}]);
+    test_correct_neighbours(room,
+                              {row: 5, col: 4},
+        [
+            {row: 4, col: 3}, {row: 2, col: 4}, {row: 4, col: 5},
+            {row: 5, col: 3},                   {row: 5, col: 5},
+            {row: 7, col: 2}, {row: 6, col: 4}, {row: 7, col: 6}
+        ]);
+});
+
+test("update in part 2", () => {
+    let room = new Room(input, new Part2());
+    expect(room.dump()).toEqual(input);
+    expect(room.update()).toBe(true);
+    expect(room.dump()).toEqual(input_with_part2_update[1]);
+    expect(room.update()).toBe(true);
+    expect(room.dump()).toEqual(input_with_part2_update[2]);
+    expect(room.update()).toBe(true);
+    expect(room.dump()).toEqual(input_with_part2_update[3]);
+    expect(room.update()).toBe(true);
+    expect(room.dump()).toEqual(input_with_part2_update[4]);
+    expect(room.update()).toBe(true);
+    expect(room.dump()).toEqual(input_with_part2_update[5]);
+    expect(room.update()).toBe(true);
+    expect(room.dump()).toEqual(input_with_part2_update[6]);
+    expect(room.update()).toBe(false);
+    expect(room.dump()).toEqual(input_with_part2_update[6]); // no change
 });
