afcda9fa17ae10ffb6c25a169e81b273d9c7f661
Michael Kopp <kopp.michael@yahoo.de>
2020-12-17 19:30:21 +0100

    add day17, part 2 (but quite ugly)

--- src/day17.ts
+++ src/day17.ts
@@ -8,9 +8,26 @@ interface Point3D
     z: number;
 }
 
-function stringify(position: Point3D): string
+interface Point4D
 {
-    return position.x.toString() + ":" + position.y.toString() + ":" + position.z.toString();
+    x: number;
+    y: number;
+    z: number;
+    w: number;
+}
+
+
+function stringify(position: Point3D | Point4D): string
+{
+    const begin = position.x.toString() + ":" + position.y.toString() + ":" + position.z.toString();
+    // No really good way to check whether position is a Point4d but the following.
+    // See https://stackoverflow.com/a/14426274/2165903.
+    if ('w' in position) {
+        return begin + ":" + position.w.toString();
+    }
+    else {
+        return begin;
+    }
 }
 
 
@@ -133,9 +150,127 @@ export class Grid3D
 }
 
 
+export class Grid4D
+{
+    cells = new Map<string, boolean>();
+    bottom_left: Point4D = {x: 0, y: 0, z: 0, w: 0};
+    top_right: Point4D = {x: 0, y: 0, z: 0, w: 0};
+
+    is_occupied(position: Point4D): boolean
+    {
+        const value = this.cells.get(stringify(position));
+        if ((value == null) || (value == false)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    set_occupation(position: Point4D, is_occupied: boolean): void
+    {
+        this.cells.set(stringify(position), is_occupied);
+
+        this.bottom_left.x = Math.min(position.x, this.bottom_left.x);
+        this.bottom_left.y = Math.min(position.y, this.bottom_left.y);
+        this.bottom_left.z = Math.min(position.z, this.bottom_left.z);
+        this.bottom_left.w = Math.min(position.w, this.bottom_left.w);
+
+        this.top_right.x = Math.max(position.x, this.top_right.x);
+        this.top_right.y = Math.max(position.y, this.top_right.y);
+        this.top_right.z = Math.max(position.z, this.top_right.z);
+        this.top_right.w = Math.max(position.w, this.top_right.w);
+    }
+
+    box_around_points(): Array<Point4D>
+    {
+        let points = new Array<Point4D>();
+        for (let w = this.bottom_left.w - 1; w <= this.top_right.w + 1; ++w) {
+            for (let z = this.bottom_left.z - 1; z <= this.top_right.z + 1; ++z) {
+                for (let y = this.bottom_left.y - 1; y <= this.top_right.y + 1; ++y) {
+                    for (let x = this.bottom_left.x - 1; x <= this.top_right.x + 1; ++x) {
+                        points.push({ x: x, y: y, z: z, w: w });
+                    }
+                }
+            }
+        }
+        return points;
+    }
+
+    get_number_of_occupied_neighbours(position: Point4D): OccupiedNeighbours
+    {
+        const possible_deltas = [-1, 0, +1];
+
+        let number_of_occupied_neighbours = 0;
+
+        for (const dw of possible_deltas) {
+            for (const dz of possible_deltas) {
+                for (const dy of possible_deltas) {
+                    for (const dx of possible_deltas) {
+                        const is_this_position = (dx === 0) && (dy === 0) && (dz === 0) && (dw === 0);
+                        if (is_this_position) {
+                            continue;
+                        }
+                        const neighbour_position = { x: position.x + dx, y: position.y + dy, z: position.z + dz, w: position.w + dw };
+                        if (this.is_occupied(neighbour_position)) {
+                            number_of_occupied_neighbours += 1;
+                        }
+                        // fastrack: we're only interested in some values
+                        if (number_of_occupied_neighbours > OccupiedNeighbours.max) {
+                            return enumify(number_of_occupied_neighbours);
+                        }
+                    }
+                }
+            }
+        }
+
+        return enumify(number_of_occupied_neighbours);
+    }
+
+    get_number_of_occupied(): number
+    {
+        let count = 0;
+        for (const cell_value of this.cells.values()) {
+            if (cell_value === true) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+}
+
+
 export function execute_conway_step(grid: Grid3D): Grid3D
 {
     const updated_grid = new Grid3D();
+    for (const point of grid.box_around_points()) {
+        const is_active = grid.is_occupied(point);
+        const active_neighbours = grid.get_number_of_occupied_neighbours(point);
+
+        let activate = false;
+        if (is_active === true) {
+            if ((active_neighbours === OccupiedNeighbours.two) || (active_neighbours === OccupiedNeighbours.three)) {
+                activate = true;
+            }
+        }
+        else {
+            if (active_neighbours === OccupiedNeighbours.three) {
+                activate = true;
+            }
+        }
+
+        if (activate) {
+            updated_grid.set_occupation(point, true);
+        }
+    }
+    return updated_grid;
+}
+
+
+export function execute_conway_step_4d(grid: Grid4D): Grid4D
+{
+    const updated_grid = new Grid4D();
     for (const point of grid.box_around_points()) {
         const is_active = grid.is_occupied(point);
         const active_neighbours = grid.get_number_of_occupied_neighbours(point);
@@ -186,6 +321,32 @@ export function parse_grid_from_strings(input: Array<string>): Grid3D
 }
 
 
+export function parse_grid_from_strings_4d(input: Array<string>): Grid4D
+{
+    let point: Point4D = {x: 0, y: 0, z: 0, w: 0};
+    let grid = new Grid4D();
+
+    for (const line of input)
+    {
+        for (const char of line) {
+            if (char === "#") {
+                grid.set_occupation(point, true);
+            }
+            else if (char === ".") {
+                // ignore
+            }
+            else {
+                throw new Error(`Unable to parse character ${char} in line ${line}.`);
+            }
+            point.x += 1;
+        }
+        point.y += 1;
+        point.x = 0;
+    }
+    return grid;
+}
+
+
 function count_active_cells_after_n_cycles(grid: Grid3D, n: number): number
 {
     let current_grid = grid;
@@ -197,9 +358,23 @@ function count_active_cells_after_n_cycles(grid: Grid3D, n: number): number
 }
 
 
+function count_active_cells_after_n_cycles_4d(grid: Grid4D, n: number): number
+{
+    let current_grid = grid;
+    for (let repetition = 0; repetition < n; ++repetition)
+    {
+        current_grid = execute_conway_step_4d(current_grid);
+    }
+    return current_grid.get_number_of_occupied();
+}
+
+
 if (require.main === module) {
     const input = read_file_of_strings("input/day17");
-    const initial_grid = parse_grid_from_strings(input);
+    const initial_grid_3d = parse_grid_from_strings(input);
 
-    console.log("Problem 1: ", count_active_cells_after_n_cycles(initial_grid, 6));
+    console.log("Problem 1: ", count_active_cells_after_n_cycles(initial_grid_3d, 6));
+
+    const initial_grid_4d = parse_grid_from_strings_4d(input);
+    console.log("Problem 2: ", count_active_cells_after_n_cycles_4d(initial_grid_4d, 6));
 }
