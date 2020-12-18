c2bf294d174eecff62ac5eb67a74dbf0f83679e3
Michael Kopp <kopp.michael@yahoo.de>
2020-12-18 22:16:39 +0100

    add day18, part 2

--- src/day18.ts
+++ src/day18.ts
@@ -1,4 +1,3 @@
-import { Stream } from "stream";
 import { read_file_of_strings } from "./read_file_utils";
 
 
@@ -10,6 +9,89 @@ function is_number(token: Token): boolean
 {
     const match = token.match(/^(\d+)$/);
     return match != null;
+}
+
+
+// Note: the first part of the problem can be solved without AST -- see git history.
+
+enum AstNodeKind
+{
+    invalid,
+    number,
+    addition,
+    multiplication,
+}
+
+abstract class AstNode
+{
+    kind: AstNodeKind = AstNodeKind.invalid;
+
+    abstract evaluate(): number;
+}
+
+class InvalidNode extends AstNode
+{
+    constructor()
+    {
+        super();
+        this.kind = AstNodeKind.invalid;
+    }
+
+    evaluate(): number
+    {
+        throw new Error("Invalid node evaluated.");
+    }
+}
+
+class AstOperation extends AstNode
+{
+    a: AstNode;
+    b: AstNode;
+    constructor(kind: AstNodeKind, a: AstNode, b: AstNode) {
+        super();
+        this.kind = kind;
+        this.a = a;
+        this.b = b;
+    }
+
+    static make_addition(a: AstNode, b: AstNode): AstOperation {
+        return new AstOperation(AstNodeKind.addition, a, b);
+    }
+
+    static make_multiplication(a: AstNode, b: AstNode): AstOperation {
+        return new AstOperation(AstNodeKind.multiplication, a, b);
+    }
+
+    evaluate(): number
+    {
+        const value_a = this.a.evaluate();
+        const value_b = this.b.evaluate();
+        switch (this.kind) {
+            case AstNodeKind.multiplication:
+                return value_a * value_b;
+            case AstNodeKind.addition:
+                return value_a + value_b;
+            default:
+                throw new Error(`Unknown node kind ${this.kind} -- no known operation.`);
+        }
+    }
+}
+
+class AstNumber extends AstNode
+{
+    value: number;
+
+    constructor(value: number)
+    {
+        super();
+        this.value = value;
+        this.kind = AstNodeKind.number;
+    }
+
+    evaluate(): number
+    {
+        return this.value;
+    }
 }
 
 
@@ -73,12 +155,46 @@ function index_of_matching_closing_parenthesis(tokens: StreamOfTokens, index_of_
 }
 
 
-function evaluate_tokens(tokens: StreamOfTokens): number {
+/*
+Build up an AST and evaluate it.
+
+Without operator precedence, just add the next operation "on top" of the
+current operation (i.e. make it the new root and have it use the old root as
+one of the members).
+In case of operator precedence, replace a member of the previously added node
+with the new node, since nodes deeper in the AST are evaluated first (as it
+should be with higher precedence).
+
+E.g.
+1 * 2 + 3 * 4
+parses is processed as follows (`^` indicates the reading position when the
+graph is modified, the AST is given in lisp syntax)
+
+1 * 2 + 3 * 4
+^
+(1)
+
+1 * 2 + 3 * 4
+    ^
+(* 1 2)
+
+1 * 2 + 3 * 4
+        ^
+(* 1 (+ 2 3))
+--> replace the 2 in the (* 1 2) with the new node (+ 2 3)
+
+1 * 2 + 3 * 4
+            ^
+(* (* 1 (+ 2 3)) 4)
+
+*/
+function evaluate_tokens(tokens: StreamOfTokens, plus_has_precedence: boolean): number {
     if (tokens.length <= 0) {
         throw new Error(`Unable to evaluate token steam of length ${tokens.length}.`);
     }
 
-    let current_value: number = NaN;  // should get overwritten in the first subexpression.
+    let root = new InvalidNode();
+    let node_of_previous_operation: AstOperation | null = null;
     let index_of_next_token_to_parse: number = 0;
 
     // we expect some tuples of operator and number|(...)
@@ -86,18 +202,18 @@ function evaluate_tokens(tokens: StreamOfTokens): number {
     let is_first_subexpression = true;
     while (index_of_next_token_to_parse < tokens.length) {
         // operator
-        let operation: (next_value: number) => number;
+        let operation: AstNodeKind;
         if (is_first_subexpression) {
-            operation = (next_value: number) => next_value;
             is_first_subexpression = false;
+            operation = AstNodeKind.number;
         }
         else {
             const operator_token = tokens[index_of_next_token_to_parse];
             if (operator_token === "*") {
-                operation = (next_value: number) => current_value * next_value;
+                operation = AstNodeKind.multiplication;
             }
             else if (operator_token === "+") {
-                operation = (next_value: number) => current_value + next_value;
+                operation = AstNodeKind.addition;
             }
             else {
                 throw new Error(`Expected operator (+ or *) but got ${operator_token}.`);
@@ -114,32 +230,67 @@ function evaluate_tokens(tokens: StreamOfTokens): number {
         else if (tokens[index_of_next_token_to_parse] === "(") {
             const index_closing_paren = index_of_matching_closing_parenthesis(tokens, index_of_next_token_to_parse);
             const sub_expression = tokens.slice(index_of_next_token_to_parse + 1, index_closing_paren);
-            next_value = evaluate_tokens(sub_expression);
+            next_value = evaluate_tokens(sub_expression, plus_has_precedence);
             index_of_next_token_to_parse = index_closing_paren + 1;
         }
         else {
             throw new Error(`Expected number or "(" as first part of an expression, not ${tokens[0]}.`);
         }
+        const next_number = new AstNumber(next_value);
 
-        current_value = operation(next_value);
+        if (operation === AstNodeKind.number) {
+            root = next_number;
+        }
+        else {
+            if (operation === AstNodeKind.addition) {
+                if (plus_has_precedence && (node_of_previous_operation != null)) {
+                    const new_operation = AstOperation.make_addition(node_of_previous_operation.b, next_number);
+                    node_of_previous_operation.b = new_operation;
+                    node_of_previous_operation = new_operation;
+                }
+                else {
+                    const new_operation = AstOperation.make_addition(root, next_number);
+                    root = new_operation;
+                    node_of_previous_operation = new_operation;
+                }
+            }
+            else if (operation === AstNodeKind.multiplication) {
+                const new_operation = AstOperation.make_multiplication(root, next_number);
+                root = new_operation;
+                node_of_previous_operation = new_operation;
+            }
+        }
     }
 
-
-    return current_value;
+    return root.evaluate();
 }
 
-export function evaluate(expression: string): number {
-    return evaluate_tokens(tokenize(expression));
+export function evaluate_without_precedence(expression: string): number {
+    return evaluate_tokens(tokenize(expression), false);
+}
+
+export function evaluate_with_plus_precedence(expression: string): number {
+    return evaluate_tokens(tokenize(expression), true);
 }
 
 
 if (require.main === module) {
     const input = read_file_of_strings("input/day18");
 
-    let sum = 0;
-    for (const line of input) {
-        sum += evaluate(line);
+    {
+        let sum = 0;
+        for (const line of input) {
+            sum += evaluate_without_precedence(line);
+        }
+        console.log("Problem 1: ", sum);
     }
-    console.log("Problem 1: ", sum);
+
+    {
+        let sum = 0;
+        for (const line of input) {
+            sum += evaluate_with_plus_precedence(line);
+        }
+        console.log("Problem 2: ", sum);
+    }
 
 }
--- tests/day18.test.ts
+++ tests/day18.test.ts
@@ -1,4 +1,8 @@
-import { tokenize, evaluate } from "../src/day18";
+import {
+    tokenize,
+    evaluate_without_precedence,
+    evaluate_with_plus_precedence,
+} from "../src/day18";
 
 
 test("Tokenize", () => {
@@ -13,17 +17,32 @@ test("Tokenize", () => {
 });
 
 
-test("Evaluate", () => {
-    expect(evaluate("5")).toBe(5);
-    expect(evaluate("(5)")).toBe(5);
-    expect(evaluate("(((5)))")).toBe(5);
-    expect(evaluate("1 + 1")).toBe(2);
-    expect(evaluate("1 + (1 + 1)")).toBe(3);
+test("Evaluate without precedence", () => {
+    expect(evaluate_without_precedence("5")).toBe(5);
+    expect(evaluate_without_precedence("(5)")).toBe(5);
+    expect(evaluate_without_precedence("(((5)))")).toBe(5);
+    expect(evaluate_without_precedence("1 + 1")).toBe(2);
+    expect(evaluate_without_precedence("1 + (1 + 1)")).toBe(3);
 
-    expect(evaluate("1 + (2 * 3) + (4 * (5 + 6))")).toBe(51);
-    expect(evaluate("2 * 3 + (4 * 5)")).toBe(26);
-    expect(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toBe(437);
-    expect(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toBe(12240);
-    expect(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toBe(13632);
-
+    expect(evaluate_without_precedence("1 + (2 * 3) + (4 * (5 + 6))")).toBe(51);
+    expect(evaluate_without_precedence("2 * 3 + (4 * 5)")).toBe(26);
+    expect(evaluate_without_precedence("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toBe(437);
+    expect(evaluate_without_precedence("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toBe(12240);
+    expect(evaluate_without_precedence("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toBe(13632);
 })
+
+
+test("Evaluate with + precedence", () => {
+    expect(evaluate_with_plus_precedence("5")).toBe(5);
+    expect(evaluate_with_plus_precedence("(5)")).toBe(5);
+    expect(evaluate_with_plus_precedence("(((5)))")).toBe(5);
+    expect(evaluate_with_plus_precedence("1 + 1")).toBe(2);
+    expect(evaluate_with_plus_precedence("1 + (1 + 1)")).toBe(3);
+
+    expect(evaluate_with_plus_precedence("1 + 2 * 3 + 4 * 5 + 6")).toBe(231);
+    expect(evaluate_with_plus_precedence("1 + (2 * 3) + (4 * (5 + 6))")).toBe(51);
+    expect(evaluate_with_plus_precedence("2 * 3 + (4 * 5)")).toBe(46);
+    expect(evaluate_with_plus_precedence("5 + (8 * 3 + 9 + 3 * 4 * 3)")).toBe(1445);
+    expect(evaluate_with_plus_precedence("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).toBe(669060);
+    expect(evaluate_with_plus_precedence("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).toBe(23340);
+});
